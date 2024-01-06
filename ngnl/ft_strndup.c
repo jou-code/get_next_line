f7e3e3c87f7a854e28fce06ed50ce7c7fcace0f4
jou.dragopc <jou.42.rio@gmail.com>
2024-01-06 06:01:44 -0300

    new gnl

---
+++ ngnl/ft_strndup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgils <jgils@student.42.rio>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 06:46:10 by jgils             #+#    #+#             */
+/*   Updated: 2023/12/13 15:07:56 by jgils            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strndup(const char *s, int n)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	if (!s || !n)
+		return (NULL);
+	dest = (char *) malloc((n + 1) * sizeof(char));
+	if (!dest)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ ngnl/get_next_line.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jou <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 17:50:36 by jou               #+#    #+#             */
+/*   Updated: 2024/01/06 01:25:10 by jou              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h> //printf
+#include <unistd.h> // write?
+//#include <limits.h> //
+#include <stdlib.h> //malloc NULL
+#include <fcntl.h> //open
+
+# define BUFFER_SIZE 4
+
+char	*ft_strdup(const char *s);
+char    *ft_strndup(const char *s, int n);
+char    *ft_strjoin(char const *s1, char const *s2);
+
+int     get_end(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str && str[i])
+	{
+                if (str[i] == '\n')
+		{
+			i++;
+			break ;
+		}
+                i++;
+        }
+        return (i);
+}
+
+char	*get_str(char *buf, char **keep)
+{
+	char	*str;
+	int	len;
+
+	len = get_end(buf);
+	if (!buf || !len)
+		return (NULL);
+	else if (buf[0] == '\n')
+		str = "\n";
+	else
+		str = ft_strndup(buf, len);
+	
+
+
+	if (buf[len] && buf[len - 1] == '\n')
+		*keep = ft_strdup(&buf[len]);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char	*buf;
+	char	*str;
+	static char	*keep;
+
+	buf = (char *) malloc ((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (NULL);
+	buf[BUFFER_SIZE] = '\0';
+
+	str = (char *) malloc (sizeof(char));
+	str[0] = 0;
+	if (!str)
+		return (NULL);
+
+	
+
+	if (keep)
+		str = get_str(keep, &keep);
+
+	while ((str[get_end(str) - 1] != '\n') && (read(fd, buf, BUFFER_SIZE) > 0))
+	{
+		if (keep)
+			str = get_str(keep, &keep);
+		else
+			str = ft_strjoin(str, get_str(buf, &keep));
+	}
+
+	free (buf);
+	return (str);
+}
+
+int     main(void)
+{
+	int     fd;
+
+        fd = open("test.txt", O_RDONLY);
+        printf("%s", get_next_line(fd));
+        printf("%s", get_next_line(fd));
+        printf("%s", get_next_line(fd));
+        return (0);
+}
+
+/*
+int	main(teste get_str)
+{
+	static char	*keep;
+	char	*buf;
+
+	buf = "";
+	keep = "teste";
+
+	printf("str-----\n%s\n", get_str(buf, &keep));
+	printf("keep-----\n%s\n", keep);
+}
+*/
